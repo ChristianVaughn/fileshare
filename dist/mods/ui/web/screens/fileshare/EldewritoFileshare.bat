2bd7cb718a49b3ae8be55281ea9424b63ab6e083
Christian Vaughn <cvaughn55@gmail.com>
2018-11-14 19:42:20 -0800

    Added dist folder and sources
    Dist folder contains stuff in the release zip, and FileshareHelper contains the Node.js code for the FileshareHelper.exe and EdFileshareLauncher contains the scripts for the EdFileshareLauncher.exe and the EldewritoFileshare.bat so you can see with your eyes what everything is and that it is not a virus

--- dist/mods/ui/web/screens/fileshare/EldewritoFileshare.bat
+++ dist/mods/ui/web/screens/fileshare/EldewritoFileshare.bat
@@ -1,6 +1,6 @@
 echo@off
 set mypath=%cd%
-START /min FileshareHelper.exe
+START /d "%mypath%\mods\ui\web\screens\fileshare\" FileshareHelper.exe
 
-START /WAIT %mypath%\..\..\..\..\..\eldorado.exe
+START /WAIT /d "%mypath%\" eldorado.exe
 tasklist | find /i "eldorado.exe"   || taskkill /im FileshareHelper.exe /F
