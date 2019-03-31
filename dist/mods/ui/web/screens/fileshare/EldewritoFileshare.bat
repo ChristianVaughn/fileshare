d6a82f9b7795737fedeee4aabec015d430f46d02
Christian Vaughn <cvaughn55@gmail.com>
2019-03-30 20:09:31 -0700

    Delete EldewritoFileshare.bat

--- dist/mods/ui/web/screens/fileshare/EldewritoFileshare.bat
+++
@@ -1,6 +0,0 @@
-echo@off
-set mypath=%cd%
-START /d "%mypath%\mods\ui\web\screens\fileshare\" FileshareHelper.exe
-
-START /WAIT /d "%mypath%\" eldorado.exe
-tasklist | find /i "eldorado.exe"   || taskkill /im FileshareHelper.exe /F
