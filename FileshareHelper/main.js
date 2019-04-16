0c158133ab09a6f2b5770f8e3ed2675effaf24da
Christian Vaughn <cvaughn55@gmail.com>
2019-04-15 18:17:46 -0700

    Add files via upload

--- FileshareHelper/main.js
+++ FileshareHelper/main.js
@@ -17,39 +17,74 @@ var fun =function(){
         console.log(data.toString());                       
     });  
 }
-function downloadImage(image_url, imageDest) {
+
+
+
+function downloadVariant(data, gametype) {
+
+    if (!fs.existsSync('../../../../../mods/variants/pmetpmet')){
+
+        fs.mkdirSync('../../../../../mods/variants/pmetpmet');
+    }
+
+    fs.writeFileSync('../../../../../mods/variants/pmetpmet/variant'+ gametype, data);
+    fun();
+     
+}
+function downloadPrefab(data, fabname) {
+
+    if (!fs.existsSync('../../../../../mods/prefabs/' )){
+
+        fs.mkdirSync('../../../../../mods/prefabs/');
+    }
+
+    fs.writeFileSync('../../../../../mods/prefabs/'+ fabname, data);
+}
+function downloadMap(image_url, data) {
+    var ran = randomstring.generate(10);
+
+    if (!fs.existsSync('../../../../../mods/maps/pmetpmet')){
+
+        fs.mkdirSync('../../../../../mods/maps/pmetpmet');
+    }
+
+    fs.writeFileSync('../../../../../mods/maps/pmetpmet/sandbox.map', data);
     download(image_url).then(data => {
        
-        fs.writeFileSync(imageDest+'/mapimage.jpg', data);
-        return;
+        fs.writeFileSync('../../../../../mods/maps/pmetpmet/mapimage.jpg', data);
+        fun();
     });
+     
 }
 function extractfiles(image_url) {
     try {
     var zip = new AdmZip("../../../../../mods/temp.zip");
     var zipEntries = zip.getEntries(); // an array of ZipEntry records
-
+    var mapcount = 0;
+    var idk = false;
+    var varcount = 0;
     zipEntries.forEach(function(zipEntry) {
         //console.log(zipEntry.toString()); // outputs zip entries information
         //console.log(zipEntry.header.size);
+        console.log (zipEntry.entryName);
        if (zipEntry.name == "sandbox.map") {
+           idk = true;
             if (zipEntry.header.size == 61440) {
-                if (zipEntry.entryName == "sandbox.map") {
-                    zip.extractEntryTo(/*entry name*/zipEntry.entryName, "../../../../../mods/maps/" +randomstring.generate(10) , true, true);
-                }
-                else {
-                    zip.extractEntryTo(/*entry name*/zipEntry.entryName, "tmp", true, true);
-                    var folderName = path.dirname("tmp/" + zipEntry.entryName);
-                    var pfolderName = path.basename(folderName);
-                    fs.copySync(folderName, "../../../../../mods/maps/" + pfolderName);
-                    downloadImage(image_url, "../../../../../mods/maps/" + pfolderName);
+               
+                    zip.extractEntryTo(/*entry name*/zipEntry.entryName, "../../../../../mods/maps/pmetpmet"+mapcount, false, true);
+                    console.log ("Extracted " +zipEntry.entryName);
+                    mapcount++;
+                  //  download(image_url).then(data => {
+                      //  console.log ("downloaded image for " +zipEntry.entryName);
 
-                      
-                    //fs.copy(folderName, "../../../../../mods/maps/");
-                    fs.removeSync('tmp');
-                    fun();
+                    //    fs.writeFileSync('../../../../../mods/maps/pmetpmet/mapimage.jpg', data);
+                     //   console.log ("downddloaded image for " +zipEntry.entryName);
+                      //     fun();
+                  //  });
+                    
 
-                }
+
+                
             }
             else {
                 haserror = true;
@@ -57,16 +92,35 @@ function extractfiles(image_url) {
         }
         else if (zipEntry.name == "variant.ctf" || zipEntry.name == "variant.slayer" || zipEntry.name == "variant.oddball"|| zipEntry.name == "variant.koth" || zipEntry.name == "variant.forge" || zipEntry.name == "variant.vip" || zipEntry.name == "variant.jugg" || zipEntry.name == "variant.terries" || zipEntry.name == "variant.assault" || zipEntry.name == "variant.zombiez") {
             if (zipEntry.header.size == 4096) {
-                if(zipEntry.entryName == "variant.ctf" || zipEntry.entryName == "variant.slayer" || zipEntry.entryName == "variant.oddball"|| zipEntry.entryName == "variant.koth" || zipEntry.entryName == "variant.forge" || zipEntry.entryName == "variant.vip" || zipEntry.entryName == "variant.jugg" || zipEntry.entryName == "variant.terries" || zipEntry.entryName == "variant.assault" || zipEntry.entryName == "variant.zombiez"){
-                    zip.extractEntryTo(/*entry name*/zipEntry.entryName, "../../../../../mods/variants/" +randomstring.generate(10) , true, true);
-                }
-                else {
-                    zip.extractEntryTo(/*entry name*/zipEntry.entryName, "../../../../../mods/variants", true, true);
-                }
+
+                    zip.extractEntryTo(/*entry name*/zipEntry.entryName, "../../../../../mods/variants/pmetpmet"+varcount, false, true);
+                    varcount++;
+                    if(!idk) {
+                        fun();
+                    }
+                
             }
             else {
                 haserror = true;
             }
+        }
+        else if (zipEntry.name.slice(-7) == ".prefab") {
+            zip.extractEntryTo(/*entry name*/zipEntry.entryName, "../../../../../mods/prefabs", false, true);
+
+        }
+        if(idk) {
+
+            console.log ("mapcount " +mapcount);
+            download(image_url).then(data => {
+                console.log ("mapcount2 " +mapcount);
+                for(i = 0; i < mapcount; i++) {
+                    console.log ("mapcount3 " +mapcount);
+
+                    fs.writeFileSync('../../../../../mods/maps/pmetpmet'+ i +'/mapimage.jpg', data);
+                }
+                fun();
+            });
+         
         }
     
     });
@@ -79,7 +133,7 @@ function extractfiles(image_url) {
 
 server = http.createServer( function(req, res) {
     
-    res.setHeader('Access-Control-Allow-Origin', 'http://christianvaughngames.com');
+    res.setHeader('Access-Control-Allow-Origin', 'http://dewshare.org');
     
     console.dir(req.param);
 
@@ -97,30 +151,24 @@ server = http.createServer( function(req, res) {
               console.log(file_url.slice(-3));
 
             if (file_url.slice(-4) == ".map") {
-                if (!fs.existsSync('../../../../../mods/tmp/')){
-
-                    fs.mkdirSync('../../../../../mods/tmp/');
-                }
-
-                fs.writeFileSync('../../../../../mods/tmp/sandbox.map', data);
-
-                var folderName = path.dirname('../../../../../mods/tmp/sandbox.map');
-                var folderName2 = path.dirname('../../../../../mods/tmp/mapimage.jpg');
-                var pfolderName = path.basename(folderName);
-                
-                var ran = randomstring.generate(10);
-                fs.copySync(folderName, "../../../../../mods/maps/" + ran);
-                downloadImage(image_url, "../../../../../mods/maps/" + ran);
-
-                //fs.copy(folderName, "../../../../../mods/maps/");
-                fs.removeSync('../../../../../mods/tmp');
-                fun();
+                downloadMap(image_url, data);
             }
-            else {
+            else if (file_url.slice(-4) == ".zip") {
 
                 fs.writeFileSync('../../../../../mods/temp.zip', data);
                 extractfiles(image_url);
             }
+            else if (file_url.slice(-7) == ".prefab"){
+                var fabname = path.basename(file_url);
+                downloadPrefab(data, fabname);
+            }
+            else {
+                var gametype = path.extname(file_url);
+
+                downloadVariant(data, gametype);
+            }
+
+            
             
             if(haserror == true) {
             res.writeHead(404, {'Content-Type': 'text/html'});
@@ -135,7 +183,6 @@ server = http.createServer( function(req, res) {
             });
         });
         req.on('end', function () {
-     
           
         });
         }
@@ -161,6 +208,6 @@ host = '127.0.0.1';
 server.listen(port, host);
 console.log('Listening at http://' + host + ':' + port);
 if(firstlaunch) {
-    fun();
+     fun();
     firstlaunch = false;
 }
